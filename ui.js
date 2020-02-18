e1dbb030fe60c3f9366431729bfbd03754404605
kkingsbe <kkingsbe@gmail.com>
2020-02-18 07:57:16 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,6 +1,16 @@
 let timeslider = document.getElementById("time-slider")
+let pausebtn = document.getElementById("pausebtn")
+
 timeslider.oninput = function(event) {
   let warpval = Math.pow(2, event.target.value)
   sim.warp = warpval
   document.getElementById("timesliderval").innerHTML = `Time Warp: ${warpval}x`
+}
+pausebtn.onclick = function() {
+  if(pausebtn.innerHTML == "Pause") {
+    pausebtn.innerHTML = "Play"
+  }
+  else {
+    pausebtn.innerHTML = "Pause"
+  }
 }
